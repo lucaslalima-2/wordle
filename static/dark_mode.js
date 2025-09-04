f7ee2e934b9421b3e2380158079d2e193c0aef36
Lucas LaLima <luke@macbookpro.mynetworksettings.com>
2025-09-03 21:06:41 -0400

    Update dark-mode text

--- static/dark_mode.js
+++ static/dark_mode.js
@@ -1,12 +1,15 @@
 // Initializes dark mode toggle
 const toggle = document.getElementById("dark-switch");
+const label = document.getElementById("dark-mode-label");
 
 // Initializes app in dark-mode
-if(toggle.checked) {
+if (toggle.checked) {
   document.body.classList.add('dark-mode');
+  label.textContent = "Light Mode";
 } // if
 
 // Sets change behavior
 toggle.addEventListener("change", () => {
   document.body.classList.toggle('dark-mode', toggle.checked);
+  label.textContent = toggle.checked ? "Light Mode" : "Dark Mode";
 }); // add event listener
